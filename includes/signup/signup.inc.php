b26472ebab7488e082843b07fe92e58d957a8971
Rutheesh <rutheeshreddychittepu@gmail.com>
2025-03-16 17:29:03 +0530

    set conditions for password

--- includes/signup/signup.inc.php
+++ includes/signup/signup.inc.php
@@ -61,6 +61,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $errors["password_length"] = "Password length should be less than 100 characters";
         }
 
+        $pattern = '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).+$/';
+
+        if(strlen($pwd) < 8 || preg_match($pattern, $pwd) === 1)
+        {
+            $errors["pwd_requirements"] = "The password should have atleast 8 characters,atleast 1 uppercase letter, atleast 1 lowercase letter, 1 digit,1 special character(@#$%^&*)";
+        }
+        
         if (strlen($email) > 320) { 
             $errors["email_length"] = "Email length should be less than 320 characters";
         }
